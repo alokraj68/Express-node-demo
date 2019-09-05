85c47faceb38c9b962dbecf75a48f2e9241eb398
Alok Rajasukumaran <alokraj68@gmail.com>
2019-09-05 11:51:02 +0530

    added api

--- webserver.js
+++ webserver.js
@@ -7,7 +7,7 @@ const port = process.env.port || process.env.PORT || 3000;
 app.use(express.static(__dirname + '/public'));
 app.get('/api', function (req, res) {
     res.send('Hello World from api!')
-  })
+})
 
 app.listen(port);
-console.log('Express File server is up in port ',port);
+console.log('Express File server is up in port ', port);
