70111e16ac54ba55db92dfb15c0ebce8f49fa8ce
Alok Rajasukumaran <alokraj68@gmail.com>
2019-09-05 11:47:33 +0530

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -5,6 +5,9 @@ var app = express();
 const port = process.env.port || process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
+app.get('/api', function (req, res) {
+    res.send('Hello World from api!')
+  })
 
 app.listen(port);
 console.log('Express File server is up in port ',port);
