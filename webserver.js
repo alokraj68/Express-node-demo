504a55857dd64f27770178e04eb9244f82409b1f
Akshay N Shaju <mail@akshaynshaju.com>
2019-10-03 22:13:53 +0530

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,8 +1,9 @@
 // Import required pckages
 var express = require('express');
 var app = express();
+const port_no = 3000;
 
-const port = process.env.port || process.env.PORT || 3000;
+const port = process.env.port || process.env.PORT || port_no;
 
 app.use(express.static(__dirname + '/public'));
 app.get('/api', function (req, res) {
